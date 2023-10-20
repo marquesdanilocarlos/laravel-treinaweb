4fcc0adacc11d76194d3248160e83b9ecb868414
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-20 11:37:03 -0300

    Aula 03 - Parâmetros de rotas

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,29 @@ Route::get('/servicos', function () {
     return "Conteúdo Serviços";
 });
 
+Route::get('/servico/{id}/{name?}', function (int $id, string $name = null) {
+    $services = [
+        1 => [
+            "name" => "Lavagem",
+            "description" => "descrição do serviço"
+        ],
+        2 => [
+            "name" => "Polimento",
+            "description" => "descrição do serviço"
+        ],
+        3 => [
+            "name" => "Secagem",
+            "description" => "descrição do serviço"
+        ],
+        "danilo" => [
+            "name" => "Default",
+            "description" => "descrição do serviço"
+        ]
+    ];
+
+    echo $services[$id]["name"] ?? $services[$name]["name"];
+});
+
 Route::get('/contato', function () {
     return "Conteúdo Contato";
 });
