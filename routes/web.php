1aa2173c40f853d18136880e64468c41ba5d4f14
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-20 17:48:05 -0300

    Aula 15 - Criação de resources (Controllers e Rotas)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\Site\GreetingController;
+use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,11 +22,14 @@ Route::get("/contato", [SiteController::class, "contact"]);
 Route::get("/servicos", [SiteController::class, "services"]);
 Route::get("/servico/{id}/{name?}", [SiteController::class, "service"]);
 
-Route::get("/clientes", [ClientController::class, 'index'])->name("clients.index");
+/*Route::get("/clientes", [ClientController::class, 'index'])->name("clients.index");
 Route::get("/clientes/create", [ClientController::class, 'create'])->name("clients.create");
 Route::get("/clientes/{id}", [ClientController::class, 'show'])->name("clients.show");
 Route::get("/clientes/{id}/edit", [ClientController::class, 'edit'])->name("clients.edit");
 Route::put("/clientes/{id}", [ClientController::class, 'update'])->name("clients.update");
 Route::post("/clientes", [ClientController::class, "store"])->name("clients.store");
-Route::delete("/clientes/{id}", [ClientController::class, "destroy"])->name("clients.destroy");
+Route::delete("/clientes/{id}", [ClientController::class, "destroy"])->name("clients.destroy");*/
+
+Route::resource('/clients', ClientController::class);
+Route::resource('/services', ServiceController::class);
 
